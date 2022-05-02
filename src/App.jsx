6a4f810a30b577338fea2e2af0f90023c2bbba0b
Deshawn Brown <brownd726@gmail.com>
2022-05-02 15:47:15 -0500

    Completed CreatePost component and instantiated it to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import React, { useState } from 'react';
 import Post from './Components/Post/Post';
-
+import CreatePost from './Components/CreatePost/CreatePost';
 
 function App() {
 
   const [posts, setPosts] = useState([{name: 'Billy Bob', comment: 'This is a test, would you not agree?'},{name: 'Sally May', comment: 'I am inevitable'}])
 
+  function addNewPost(post){
+
+    let tempPosts = [post, ...posts];
+
+    setPosts(tempPosts);
+  }
+
   return (
     <div >
         <h3>Social Feed</h3>
         <div>
+          <CreatePost addNewPostProperty={addNewPost}/>
           <Post parentPosts={posts}/>
         </div>
     </div>
--- src/Components/CreatePost/CreatePost.jsx
+++ src/Components/CreatePost/CreatePost.jsx
@@ -4,26 +4,27 @@ import React, { useState } from "react";
 const CreatePost = (props) => {
 
     const [name, setName] = useState('');
-    const [userPost, setUserPost] = useState('');
+    const [comment, setComment] = useState('');
 
     function handleSubmit(event) {
-        event.preventDefalt();
+        event.preventDefault();
         let newPost = {
             name : name,
-            userPost : userPost
+            comment : comment
         };
         console.log(newPost);
-        props.addNewPost(newPost)
+        props.addNewPostProperty(newPost)
     }
 
     return (
-        <form onSubmit={handleSubmit} className='form-grid'>
-            <div className="Form-group">
-                <label>Name</label>
-                <input type='text' className="form-control" value={name} onChange{(event) => setName(parseFloat(event.target.value))}/>
-                
-            </div>
-
+        <form onSubmit={handleSubmit}>
+            <label>Name</label>
+            <input type='text' value={name} onChange={(event) =>setName(event.target.value)}/>
+            <label>Post</label>
+            <input type='text'value={comment} onChange={(event) =>setComment(event.target.value)}/>
+            <button type="submit">Create</button>
         </form>
-    )
+    );
 }
+
+export default CreatePost;
